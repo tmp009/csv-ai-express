10d50f278ab2503b06507670685959a2b246ac74
tmp009 <128413103+tmp009@users.noreply.github.com>
2024-01-24 12:46:10 +0200

    use json object

--- sendPromptCSV.js
+++ sendPromptCSV.js
@@ -6,13 +6,15 @@ const openai = new OpenAI();
 
 async function prompt(message, csv) {
     const messages = [
-        {role:'system', content: 'You will be given CSV data and the user will ask you to something with it. Returns the data in a JSON array'}, 
+        {role:'system', content: 'You will be given CSV data and the user will ask you to something with it. Return the data in a JSON array in a object with the key "data".'},
+        {role:'system', content: 'If the question or task is vague then return an empty array.'},
         {role:'user', content: csv},
         {role:'user', content: message}
     ]
     const completion = await openai.chat.completions.create({
         messages,
-        model: "gpt-4",
+        model: "gpt-4-1106-preview",
+        response_format: {type: 'json_object'}
     })
 
     return completion.choices[0].message.content
